9885699739f840ff53b8abb521ce7e294c549064
Maria Helenius <maheleni@c2r5p7.hive.fi>
2024-09-16 16:13:10 +0300

    Resolved conflicts

--- sources/marias_main.c
+++ sources/marias_main.c
@@ -4,9 +4,14 @@
 /*   marias_main.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: maheleni <maheleni@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: maheleni <maheleni@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/12 11:21:42 by maheleni          #+#    #+#             */
+<<<<<<< Updated upstream
 /*   Updated: 2024/09/16 16:08:42 by maheleni         ###   ########.fr       */
+=======
+/*   Updated: 2024/09/13 13:18:02 by maheleni         ###   ########.fr       */
+>>>>>>> Stashed changes
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +21,7 @@
 
 int	main(int argc, char *argv[], char *envp[])	//main to test stuff with
 {
+<<<<<<< Updated upstream
 	char *line = readline("prompt: ");
 	printf(":%s\n", line);
 	// printf("Envp\n");
@@ -28,4 +34,17 @@ int	main(int argc, char *argv[], char *envp[])	//main to test stuff with
 	// char *args[] = {"bash", NULL};
 	// if (execve("/bin/bash", args, envp) == 0)
 	// 	printf("Problem with execv\n");
+=======
+	printf("Envp\n");
+	int i = 0;
+	while (envp[i] != NULL)
+	{
+		printf("%s\n", envp[i]);
+		i++;
+	}
+    sleep(10);
+	char *args[] = {"bash", NULL};
+	if (execve("/bin/bash", args, envp) == 0)
+		printf("Problem with execv\n");
+>>>>>>> Stashed changes
 }
