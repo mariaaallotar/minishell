e9a409bf7581eb65ad9018751cb704365d64c5a3
Maria <maria.helenius99@gmail.com>
2024-09-26 10:49:51 +0300

    Made ready to merge

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: maheleni <maheleni@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/11 10:12:47 by eberkowi          #+#    #+#             */
-/*   Updated: 2024/09/20 15:40:10 by maheleni         ###   ########.fr       */
+/*   Updated: 2024/09/26 10:48:40 by maheleni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,30 +30,18 @@ int	main(int argc, char *argv[], char *envp[]) //what happens if ./minishell get
 	(void)*argv;
 	initialize_variables(&main, &commands);
 	copy_env(envp, &main);
-
-
-	// // update_env(&main);	//do we do this at all?
-	// //create_signals();
-	// while (1)
-	// {
-	// 	// handle_inputs(&main.input);
-	// 	// parsing(&main, &commands);
-	// 	//apply_commands_in_tree();
-	// 	//set_exit_status_of_last_line();
-	// 	break;
-	// }
-	// if (main.input)
-	// 	free(main.input);
-
-	print_linked_list(main.env_list);
-
-	chdir("..");							//works without PWD
-	char *my_argv[] = {"ls", NULL};
-	// char *buf = malloc (100);
-	// printf("\n%s\n", getcwd(buf, 100));		//works even without PWD
-	pwd();
-	execve("/bin/ls", my_argv, envp);
-
+	// update_env(&main);	//do we do this at all?
+	//create_signals();
+	while (1)
+	{
+		// handle_inputs(&main.input);
+		// parsing(&main, &commands);
+		//apply_commands_in_tree();
+		//set_exit_status_of_last_line();
+		break;
+	}
+	if (main.input)
+		free(main.input);
 	free_environment(&(main.env_list));
 	//free_signals();
 	exit (main.exit_code);
