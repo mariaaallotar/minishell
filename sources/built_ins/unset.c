52af9fe22fbd8258d3e64f473a44a0866b568b98
Maria <maria.helenius99@gmail.com>
2024-12-02 10:57:52 +0200

    Unset no longer does anything if the argument contains '='

--- sources/built_ins/unset.c
+++ sources/built_ins/unset.c
@@ -6,7 +6,7 @@
 /*   By: maheleni <maheleni@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 09:33:42 by maheleni          #+#    #+#             */
-/*   Updated: 2024/11/27 16:15:02 by maheleni         ###   ########.fr       */
+/*   Updated: 2024/12/02 10:56:02 by maheleni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,20 @@
 int	unset(t_main *main, t_tokens token)
 {
 	int		i;
-	char	*temp_variable;
 
 	i = 1;
 	if (token.command[1] == NULL)
 		return (0);
-	temp_variable = ft_strjoin(token.command[i], "=");
-	if (temp_variable == NULL)
-	{
-		perror("unset");
-		return (1);
-	}
 	while (token.command[i] != NULL)
 	{
-		if (find_node(main, temp_variable) == NULL)
+		if (ft_strchr(token.command[i], '=') != NULL
+			|| find_node(main, token.command[i]) == NULL)
 		{
 			i++;
 			continue ;
 		}
-		remove_variable(main, temp_variable);
+		remove_variable(main, token.command[i]);
 		i++;
 	}
-	free(temp_variable);
 	return (0);
 }
